68d15e10fb874bec39b9128cb882691ca997fdba
Charles Callender <charlescallender.cc@gmail.com>
2022-03-19 11:35:26 -0700

    Added detail component

--- disney-plus-clone/src/App.js
+++ disney-plus-clone/src/App.js
@@ -2,13 +2,33 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Detail from './components/Detail';
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header />
-      <Home />
+      <Routes>
+        {/* <Route path="/detail">
+          <Detail />
+        </Route> */}
+        <Route path="/detail" element={<Detail />} /> 
+        <Route path="/" element={<Home />} /> 
+        {/* <Route path="/">
+          <Home />
+        </Route> */}
+      </Routes>
+      </Router>
+      
+      
     </div>
   );
 }
