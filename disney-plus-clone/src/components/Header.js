323382e3effc3ca7c8681332a636a568356ab634
Charles Callender <charlescallender.cc@gmail.com>
2022-03-20 13:28:54 -0700

    Added login/logout feature with google

--- disney-plus-clone/src/components/Header.js
+++ disney-plus-clone/src/components/Header.js
@@ -1,12 +1,77 @@
 import React from 'react'
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import {
+    selectUserName,
+    selectUserPhoto,
+    setUserLogin,
+    setSignOut
+} from "../features/user/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import 'firebase/compat/auth';
+import { auth, provider } from '../firebase';
+import { useHistory } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+/*import 'firebase/compat/provider';*/
+/*import "firebase/compat/";*/
+
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const userName = useSelector(selectUserName);
+    const userPhoto = useSelector(selectUserPhoto);
+    /*const history = useHistory();*/
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if(user) {
+                dispatch(setUserLogin({
+                    name: user.displayName,
+                    email: user.email,
+                    photo: user.photoURL
+                }));
+                navigate('/');
+            }
+        })
+    }, [])
+
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+        .then((result) => {
+            console.log(result);
+
+            let user = result.user
+            dispatch(setUserLogin({
+                name: user.displayName,
+                email: user.email,
+                photo: user.photoURL
+            }));
+            /*history.push("/");*/
+            navigate('/');
+        })
+    }
+
+    const signOut = () => {
+        auth.signOut()
+        .then(() => {
+            dispatch(setSignOut());
+            /*history.push("/login");*/
+            navigate('/login');
+        })
+    }
+
   return (
     <Nav>
         <Logo src="/images/logo.svg" />
-        <NavMenu>
+        { !userName ? (
+            <LoginContainer>
+            <Login onClick={signIn}>Login</Login> 
+            </LoginContainer>
+            ) : (
+            <>
+            <NavMenu>
             <a>
                 <Link to={`/`}>
                 <img src='/images/home-icon.svg'/>
@@ -35,7 +100,11 @@ const Header = () => {
             </a>
 
         </NavMenu>
-        <UserImg src='/images/cj.jfif'/>
+        <UserImg onClick={signOut} src='/images/cj.jfif'/>
+            </>
+            )
+        }
+        
 
         </Nav>
   )
@@ -108,4 +177,28 @@ const UserImg = styled.img`
     height: 48px;
     border-radius: 50%;
     cursor: pointer;
+`;
+
+const Login = styled.div`
+    border: 1px solid #f9f9f9;
+    padding: 8px 16px;
+    border-radius: 4px;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    background-color: rgba(0, 0, 0, 0.6);
+    transition: all 0.2s;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+        
+    }
+`;
+
+const LoginContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
 `;
